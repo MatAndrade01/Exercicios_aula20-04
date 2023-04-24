c8acb1dd552ca515bfb8f10768259d96ac438280
Matheus Andrade <ltbmatheus141012@gmail.com>
2023-04-24 16:34:10 -0300

    new commit
    Simplificando o código

--- Exercício1049.py
+++ Exercício1049.py
@@ -1,50 +1,28 @@
 if __name__ == '__main__':
-    vertebrado_ou_ivertebrado = int(input('O seu animal é vertebrado(1) ou invertebrado(2)?'))
+    A = str(input('Digite a primeira palavra:')).lower()
+    B = str(input('Digite a segunda palavra:')).lower()
+    c = str(input('Digite a terceira palavra:')).lower()
 
-    if vertebrado_ou_ivertebrado == 1:
-        print('Seu animal é vertebrado.')
-        classe = int(input('O seu animal pertence a qual classe? Ave(1) ou Mamífero(2):'))
+    if (A == 'vertebrado') and (B == 'ave') and (c == 'carnivoro'):
+        print('Aguia')
 
-        if classe == 1:
-            print('A classe do seu animal é Ave.')
-            alimentacao = int(input('Seu animal é Carnivoro(1) ou Onivoro(2)?'))
+    if (A == 'vertebrado') and (B == 'ave') and (c == 'onivoro'):
+        print('Pomba')
 
-            if alimentacao == 1:
-                print('Seu animal é uma Aguia!')
+    if (A == 'vertebrado') and (B == 'mamifero') and (c == 'onivoro'):
+        print('Homem')
 
-            if alimentacao == 2:
-                print('Seu animal é uma Pomba!')
+    if (A == 'vertebrado') and (B == 'mamifero') and (c == 'herbivoro'):
+        print('Vaca')
 
-        if classe == 2:
-            print('A classe do seu animal é Mamífero.')
-            alimentacao = int(input('Seu animal é Onivoro(1) ou Herbivoro(2)?'))
+    if (A == 'invertebrado') and (B == 'inseto') and (c == 'hematofago'):
+        print('Pulga')
 
-            if alimentacao == 1:
-                print('Seu animal é um Homem!')
+    if (A == 'Invertebrado') and (B == 'inseto') and (c == 'herbivoro'):
+        print('Largata')
 
-            if alimentacao == 2:
-                print('Seu animal é uma Vaca!')
+    if (A == 'Invertebrado') and (B == 'anelidio') and (c == 'hematofago'):
+        print('Sanguessuga')
 
-    if vertebrado_ou_ivertebrado == 2:
-        print('Seu animal é Invertebrado.')
-        classe = int(input('O seu animal pertence a qual classe? Inseto(1) ou Anelidio(2)'))
-
-        if classe == 1:
-            print('A classe do seu animal é Inseto.')
-            alimentacao = int(input('Seu animal é Hematofago(1) ou Herbivoro(2)?'))
-
-            if alimentacao == 1:
-                print('Seu animal é uma Pulga!')
-
-            if alimentacao == 2:
-                print('Seu animal é uma Largata!')
-
-        if classe == 2:
-            print('A classe do seu animal é Anelidio.')
-            alimentacao = int(input('Seu animal é Hematofago(1) ou Onivoro(2)?'))
-
-            if alimentacao == 1:
-                print('Seu animal é uma Sanguessuga!')
-
-            if alimentacao == 2:
-                print('Seu animal é uma Minhoca!')
+    if (A == 'Invertebrado') and (B == 'anelidio') and (c == 'onivoro'):
+        print('minhoca')
